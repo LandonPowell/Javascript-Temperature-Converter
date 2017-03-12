bc9540b8737f3bd36dd0afea662749692a9a8317
Landon Powell <landonjpowell@airmail.cc>
2017-03-11 19:06:17 -0500

    Update script.js

--- MinimalClock/script.js
+++ MinimalClock/script.js
@@ -1,9 +1,14 @@
+var textTime = document.getElementById("textTime");
+var second = document.getElementById("second");
+var minute = document.getElementById("minute");
+var hour = document.getElementById("hour");
+
 setInterval(function(){
   var seconds = new Date().getSeconds();
   var minutes = new Date().getMinutes();
   var  hours  = new Date().getHours();
-  $(".textTime").html(hours+":"+minutes+"<span class='seconds'>"+seconds+"</span>" );
-  $('.second').css({ 'transform': 'rotate(' + seconds/60*360 + 'deg)' });
-  $('.minute').css({ 'transform': 'rotate(' + minutes/60*360 + 'deg)' });
-  $( '.hour' ).css({ 'transform': 'rotate(' +   hours/60*360 + 'deg)' });
+  textTime.innerHTML = hours+":"+minutes+"<span id='seconds'>+"+seconds+"</span>";
+  second.style.transform = 'rotate(' + seconds/60*360 + 'deg)';
+  minute.style.transform = 'rotate(' + minutes/60*360 + 'deg)';
+  hour  .style.transform = 'rotate(' +   hours/60*360 + 'deg)';
 }, 1000);
